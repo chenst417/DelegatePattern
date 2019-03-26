6aa08f1d1ae4764e49dceb9c63a66c9966b9b5d3
chenst <861293572@qq.com>
2019-03-26 20:43:31 +0800

    委派模式

--- src/main/java/com/cn/study/delegatepattern/mvc/DispatcherServlet.java
+++ src/main/java/com/cn/study/delegatepattern/mvc/DispatcherServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//spring mvc
 public class DispatcherServlet extends HttpServlet {
 
     @Override
