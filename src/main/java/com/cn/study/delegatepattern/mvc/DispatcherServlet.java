6d5a0b7a2022692f243dd9ac46822f117471414e
chenst <861293572@qq.com>
2019-03-27 16:38:47 +0800

    委派模式

--- src/main/java/com/cn/study/delegatepattern/mvc/DispatcherServlet.java
+++ src/main/java/com/cn/study/delegatepattern/mvc/DispatcherServlet.java
@@ -5,9 +5,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 //spring mvc
 public class DispatcherServlet extends HttpServlet {
+
+    private List<Handler> handlerMappings = new ArrayList<Handler>();
+
+    @Override
+    public void init() throws ServletException {
+
+        Class<?> memberControllerClass = MemberController.class;
+
+        try{
+            handlerMappings.add(new Handler().setController(memberControllerClass.newInstance())
+                    .setMethod(memberControllerClass.getMethod("getMemberById", new Class[]{String.class}))
+                    .setUrl("/web/getMemberById.json"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,7 +37,7 @@ public class DispatcherServlet extends HttpServlet {
 
     private void doDispatch(HttpServletRequest req, HttpServletResponse resp){
 
-        String uri = req.getRequestURI();
+        /*String uri = req.getRequestURI();
         String mid = req.getParameter("mid");
 
         if (uri.equals("getMemberById")) {
@@ -27,7 +46,63 @@ public class DispatcherServlet extends HttpServlet {
             new OrderController().getOrderById(mid);
         } else if (uri.equals("logout")) {
             new SystemController().logout();
+        } else {
+            try{
+                resp.getWriter().write(" 404 Not Found!!");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }*/
+
+        String uri = req.getRequestURI();
+        Handler handler = null;
+        for (Handler h: handlerMappings) {
+            if(h.getUrl().equals(uri)){
+                handler = h;
+                break;
+            }
+        }
+        try{
+            Object obj = handler.method.invoke(handler.getController(), req.getParameter("mid"));
+            resp.getWriter().write(obj.toString());
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
     }
+
+    class Handler {
+
+        private Object controller;
+        private String url;
+        private Method method;
+
+        public Object getController() {
+            return controller;
+        }
+
+        public Handler setController(Object controller) {
+            this.controller = controller;
+            return this;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public Handler setUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Method getMethod() {
+            return method;
+        }
+
+        public Handler setMethod(Method method) {
+            this.method = method;
+            return this;
+        }
+    }
+
 }
